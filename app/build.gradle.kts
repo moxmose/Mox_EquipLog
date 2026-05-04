746f8a0e35391ec94be06949627901fd62db86af
moxmose <moxmose@gmail.com>
2026-05-04 16:26:34 +0200

    feat: Integrate Google Services into the project
    This commit adds the necessary plugins and configuration files to support Google Services and Firebase integration.
    
    - **Project Configuration:**
        - **`libs.versions.toml`**: Defined the `google-services` plugin and added version `4.4.2` to the version catalog.
        - **`build.gradle.kts`**: Registered the `google-services` plugin in the root build file and applied it to the app module.
    - **Google Services Integration:**
        - **`google-services.json`**: Added the Google Services configuration file containing project metadata and API keys required for Firebase and Google API connectivity.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,6 +3,7 @@ plugins {
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
     alias(libs.plugins.ksp)
+    alias(libs.plugins.google.services)
     kotlin("plugin.serialization") version "2.0.21"
     id("jacoco")
 }
